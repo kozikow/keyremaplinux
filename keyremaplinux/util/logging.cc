899b12b021a4ab64862cc0c9c5c42431007638e0
Robert Kozikowski <robertkoz@google.com>
2015-12-04 14:43:19 +0000

    Define log level that disables INFO logging.

--- keyremaplinux/util/logging.cc
+++ keyremaplinux/util/logging.cc
@@ -1,4 +1,5 @@
 #include "logging.h"
+#define LOG_MIN_LEVEL WARNING
 
 namespace keyremaplinux {
 
@@ -6,8 +7,10 @@ LogMessage::LogMessage(const char* fname, int line, int severity)
     : fname_(fname), line_(line), severity_(severity) {}
 
 void LogMessage::GenerateLogMessage() {
-  fprintf(stderr, "[%c %s:%d] %s\n", "IWEF"[severity_], fname_, line_,
-          str().c_str());
+  if (severity_ >= LOG_MIN_LEVEL) {
+    fprintf(stderr, "[%c %s:%d] %s\n", "IWEF"[severity_], fname_, line_,
+        str().c_str());
+  }
 }
 
 LogMessage::~LogMessage() { GenerateLogMessage(); }
